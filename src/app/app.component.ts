37afdae9d9a04d2aec3145c3e2d2c3d4029537b7
qintb <qintb@hua-cloud.com.cn>
2018-12-18 19:40:16 +0800

    轮播图

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+  constructor(
+    private iconService: NzIconService
+  ) {
+    // 引入iconfont.cn上的自定义图标
+    this.iconService.fetchFromIconfont({
+      scriptUrl: '//at.alicdn.com/t/font_578343_h8jjpv6bmec.js'
+    });
+  }
+
   title = 'tamBlogHtml';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  bannerArr: Array<any> = [
+    {
+      class: 'bg1',
+      title: '1',
+      subtitle: ''
+    },
+    {
+      class: 'bg2',
+      title: '2',
+      subtitle: ''
+    },
+    {
+      class: 'bg3',
+      title: '3',
+      subtitle: ''
+    }
+  ];
 
   constructor() { }
 
